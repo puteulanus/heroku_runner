d7c8c90bd905230fca811f527a2ccf9ae4ee8a2e
Puteulanus <mj@puteulanus.com>
2016-05-21 14:55:02 +0800

    add init script

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=$1
+
+mkdir heroku_init
+cd heroku_init
+
+for i in {1..5}
+do
+	git clone https://github.com/heroku/php-getting-started.git
+	cd php-getting-started
+	heroku create ${name}${i}
+	git push heroku master
+	cd ..
+	rm -rf php-getting-started
+done
