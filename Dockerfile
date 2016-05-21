d4b9d5a16c6c817577f2e813486454c93fa17b3b
Puteulanus <mj@puteulanus.com>
2016-05-21 12:48:32 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM centos:7
+RUN yum install -y wget ruby screen vim which sudo
+RUN wget -O- https://toolbelt.heroku.com/install.sh | sh
+ADD run.sh /root/run.sh
+CMD 'source ~/.profile; bash /root/run.sh'
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# load mail and password
+mail=$HEROKU_EMAIL
+passwd=$(awk -F[@] '{print $1}')
+cmd=$CMD_URL
+
+if [ -z $mail ] || [ -z $passwd ] || [ -z $cmd ]
+then
+	exit
+fi
+
+# login
+(sleep 5; echo $mail; sleep 1; echo $passwd) | heroku login
+
+# loop to create screen
+for i in {1..5}
+do
+	screen -dm -S $i "while true; do (sleep 5; echo 'while true; do wget -O- ${cmd} | bash; done') | heroku run bash --app ${passwd}${i}; done"
+done
